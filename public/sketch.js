04b66572f78ab4a81e835c83e1206c8d0fbf52da
Anuj Sachan <asachan@sapient.com>
2017-05-12 22:05:05 +0530

    remove port

--- public/sketch.js
+++ public/sketch.js
@@ -6,7 +6,7 @@ function setup() {
   background(0);
   // Start a socket connection to the server
   // Some day we would run this server somewhere else
-  socket = io.connect('http://lamborghini.herokuapp.com:9001');
+  socket = io.connect('http://lamborghini.herokuapp.com');
   // We make a named event called 'mouse' and write an
   // anonymous callback function
   socket.on('mouse',
